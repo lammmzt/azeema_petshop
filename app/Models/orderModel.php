7aa288821ff3373623770d55f30038e995b14838
lammmzt <alam0098@stmik-wp.ac.id>
2025-08-11 09:18:48 +0700

    add fiture

--- app/Models/orderModel.php
+++ app/Models/orderModel.php
@@ -37,7 +37,8 @@ class orderModel extends Model
         return $this
         ->select('order.*, users.nama_user, users.username, users.no_hp_user, users.alamat_user, order.tanggal_order as tanggal')
         ->join('users', 'users.id_user = order.id_user')
-        ->where(['order.tanggal_order >=' => $tgl_awal, 'order.tanggal_order <=' => $tgl_akhir])
+        ->where('order.tanggal >=', $tgl_awal)
+        ->where('order.tanggal <=', $tgl_akhir)
         ->orderBy('id_order', 'DESC')
         ->findAll();
         
